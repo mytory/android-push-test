f66ab6b34824f00e482132811f9583e7552c4917
mytory <mytory@gmail.com>
2018-09-12 16:25:34 +0900

    remove notification object, use only data object.

--- Push.php
+++ Push.php
@@ -15,54 +15,61 @@ class Push
 
     public static function push($body): array
     {
-        $client = new Client();
-        $client->setApiKey(Config::FCM_KEY);
 
-        $message = new Message();
-        $message->setPriority('high');
+        $message = self::messageCommon($body);
 
         foreach (Config::$target_tokens as $token) {
             $message->addRecipient(new Device($token));
         }
 
-        $message
-            ->setNotification(new Notification('Android Push Test', $body))
-            ->setData(['url' => 'https://google.com'])
-        ;
+        return self::send($message);
 
-        $response = $client->send($message);
-
-        return [
-            'statusCode' => $response->getStatusCode(),
-            'contents' => \GuzzleHttp\json_decode($response->getBody()->getContents()),
-        ];
     }
 
     public static function pushToChannel($body): array
     {
-        $client = new Client();
-        $client->setApiKey(Config::FCM_KEY);
-
-        $message = new Message();
-        $message->setPriority('high');
+        $message = self::messageCommon($body);
 
         foreach (Config::$target_topics as $target_topic) {
             $message->addRecipient(new Topic($target_topic));
         }
 
-        $message
-            ->setNotification(new Notification('Android Push Test', $body))
-            ->setData(['url' => 'https://google.com'])
-        ;
+        return self::send($message);
 
+    }
+
+    private static function messageCommon($body): Message
+    {
+        $message = new Message();
+        $message->setPriority('high');
+
+        // Use only data for same notification service even if app is in background.
+        // Don't use notification object.
+        $message
+//            ->setNotification(new Notification('Android Push Test', $body))
+            ->setData([
+                'title' => 'Android Push Test',
+                'body' => $body,
+                'url' => 'https://google.com'
+            ])
+        ;
+        return $message;
+    }
+
+    /**
+     * @param $message
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    private static function send($message): array
+    {
+        $client = new Client();
+        $client->setApiKey(Config::FCM_KEY);
         $response = $client->send($message);
 
         return [
             'statusCode' => $response->getStatusCode(),
             'contents' => \GuzzleHttp\json_decode($response->getBody()->getContents()),
         ];
-
     }
-
 
 }
